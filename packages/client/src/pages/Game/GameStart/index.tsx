6169afc385596ab90dfd25dcf28fa0bb8940ffd9
chezeee <chezeee@gmail.com>
2024-09-24 19:53:45 +0300

    Стили переписаны под scss, добавлен sass, поправлена деструктуризация импортов из React

--- packages/client/src/pages/Game/GameStart/index.tsx
+++ packages/client/src/pages/Game/GameStart/index.tsx
@@ -1,32 +1,34 @@
-import React from 'react'
-import './gameStart.css'
+import { useEffect, FC } from 'react';
+import style from './GameStart.module.scss';
 
 interface GameStartProps {
-  onStart: () => void
-  onBackToMenu: () => void
+    onStart: () => void;
+    onBackToMenu: () => void;
 }
 
-const GameStart: React.FC<GameStartProps> = ({ onStart, onBackToMenu }) => {
-  React.useEffect(() => {
-    window.addEventListener('keypress', onStart)
-    return () => {
-      window.removeEventListener('keypress', onStart)
-    }
-  }, [])
+const GameStart: FC<GameStartProps> = ({ onStart, onBackToMenu }) => {
+    useEffect(() => {
+        window.addEventListener('keypress', onStart);
+        return () => {
+            window.removeEventListener('keypress', onStart);
+        };
+    }, []);
 
-  return (
-    <section className="game-start">
-      <div className="game-start-wrap">
-        <h2 className="game-start-title">Начать игру</h2>
-        <p className="blinking-text">
-          Нажмите любую клавишу, чтобы начать игру
-        </p>
-        <button className="back-to-menu-btn" onClick={onBackToMenu}>
-          Выйти в главное меню
-        </button>
-      </div>
-    </section>
-  )
-}
+    return (
+        <section className={style['game-start']}>
+            <div className={style['game-start__wrap']}>
+                <h2 className={style['game-start__title']}>Начать игру</h2>
+                <p className={style['blinking-text']}>
+                    Нажмите любую клавишу, чтобы начать игру
+                </p>
+                <button
+                    className={style['back-to-menu-btn']}
+                    onClick={onBackToMenu}>
+                    Выйти в главное меню
+                </button>
+            </div>
+        </section>
+    );
+};
 
-export default GameStart
+export default GameStart;
