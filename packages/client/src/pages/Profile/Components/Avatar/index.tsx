d26bf762dedf7abed8310f0237a521731e18b0c3
Timofey Pichugin <tpich87@gmail.com>
2024-09-26 21:57:34 +0300

    feat: update

--- packages/client/src/pages/Profile/Components/Avatar/index.tsx
+++ packages/client/src/pages/Profile/Components/Avatar/index.tsx
@@ -1,14 +1,19 @@
 import styles from './avatar.module.scss';
 import goldIcon from '../../../../assets/images/gold-icon.png';
+import iconEdit from '../../../../assets/images/icon-edit.png';
 
 type TProps = {
     link?: string;
+    handleClick?: () => void;
 };
 
-function Avatar({ link = goldIcon }: TProps): JSX.Element {
+function Avatar({ link = goldIcon, handleClick }: TProps): JSX.Element {
     return (
-        <div className={styles.container}>
-            <img src={link} alt="avatar" />
+        <div className={styles.container} onClick={handleClick}>
+            <img className={styles.avatar} src={link} alt="avatar" />
+            <span className={styles.avatarChange}>
+                <img className={styles.icon} src={iconEdit} alt="icon" />
+            </span>
         </div>
     );
 }
