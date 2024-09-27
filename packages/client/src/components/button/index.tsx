a892e8c04bd66b74b105b5e7350567d138948d80
chezeee <chezeee@gmail.com>
2024-09-27 22:11:30 +0300

    Влил изменения из dev в main-page-layout

--- packages/client/src/components/button/index.tsx
+++ packages/client/src/components/button/index.tsx
@@ -1,16 +1,16 @@
-import styles from './button.module.scss'
+import styles from './button.module.scss';
 
 type TProps = {
-  text: string
-  onClick?: () => void
-}
+    text: string;
+    onClick?: VoidFunction;
+};
 
 function Button({ text, onClick }: TProps) {
-  return (
-    <button className={`${styles.button}`} onClick={onClick}>
-      {text}
-    </button>
-  )
+    return (
+        <button className={`${styles.button}`} onClick={onClick}>
+            {text}
+        </button>
+    );
 }
 
-export default Button
+export default Button;
