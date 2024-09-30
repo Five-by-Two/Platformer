e6aed6cf691ae88d4bc1f78555a35f147ad8b98f
anillen <daniil_telenkov@vk.com>
2024-09-30 13:09:49 +0300

    add signup to form

--- packages/client/src/services/AuthService/Models/SignUpModel.ts
+++ packages/client/src/services/AuthService/Models/SignUpModel.ts
@@ -1,7 +1,7 @@
 export type SignUpModel = {
     email: string;
     first_name: string;
-    last_name: string;
+    second_name: string;
     phone: string;
     login: string;
     password: string;
