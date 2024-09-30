443c3f1835b60802578f6fa80398ca30fbc610bb
anillen <daniil_telenkov@vk.com>
2024-09-30 10:40:20 +0300

    add store provider in app

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './router';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <Router />
+                <Provider store={store}>
+                    <Router />
+                </Provider>
             </BrowserRouter>
         </div>
     );
