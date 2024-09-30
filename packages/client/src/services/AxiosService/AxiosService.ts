6c180f8100b4536a08933d47c42d571fa6e04d08
anillen <daniil_telenkov@vk.com>
2024-09-30 09:36:29 +0300

    add base api in axios

--- packages/client/src/services/AxiosService/AxiosService.ts
+++ packages/client/src/services/AxiosService/AxiosService.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
+import { BASE_API_URL } from './Models/Constants';
 
-const AxiosService = axios.create();
+const AxiosService = axios.create({
+    baseURL: BASE_API_URL,
+});
 
 export default AxiosService;
