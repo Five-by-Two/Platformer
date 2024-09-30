01bb2fd71163c91ecb555e473001108b2e3a9317
anillen <daniil_telenkov@vk.com>
2024-09-30 09:51:12 +0300

    add auth service

---
+++ packages/client/src/services/AuthService/AuthService.ts
@@ -0,0 +1,18 @@
+import { AxiosError } from 'axios';
+import AxiosService from '../AxiosService/AxiosService';
+import { SignInModel } from './Models/SignInModel';
+
+class AuthService {
+    async SignIn(model: SignInModel): Promise<boolean> {
+        return AxiosService.post('auth/signin', JSON.stringify(model))
+            .then(() => {
+                return true;
+            })
+            .catch((ex: AxiosError) => {
+                console.error(ex);
+                return false;
+            });
+    }
+}
+
+export default new AuthService();
