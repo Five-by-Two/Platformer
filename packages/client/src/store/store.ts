9778347d18ddea0d0b276e23c410c2d7b20331a9
Timofey Pichugin <tpich87@gmail.com>
2024-10-01 18:29:46 +0300

    bugfix: исправлены недочеты с ревью

--- packages/client/src/store/store.ts
+++ packages/client/src/store/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './authSlice';
 import userReducer from './userSlice';
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer,
         user: userReducer,
     },
 });
