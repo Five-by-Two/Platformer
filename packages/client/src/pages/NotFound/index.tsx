88546ae4ef1b806ad5028bda0306fdc5f7f2c18d
Pavel_Zhandin <67227912+PavelZhandin@users.noreply.github.com>
2024-10-08 16:50:47 +0300

    страница ошибки 500  (#63)
    * страница 404, новые изображения для фона
    
    * страница серверной ошибки

--- packages/client/src/pages/NotFound/index.tsx
+++ packages/client/src/pages/NotFound/index.tsx
@@ -1,3 +1,27 @@
+import { useNavigate } from 'react-router';
+import Button from '../../components/button';
+import styles from './index.module.scss';
+import { Link } from 'react-router-dom';
+
 export function NotFoundPage(): JSX.Element {
-    return <h1>404</h1>;
+    const navigate = useNavigate();
+
+    const handleClick = (): void => {
+        navigate('..');
+    };
+
+    return (
+        <div className={`${styles.container}`}>
+            <section className={`${styles.content_section}`}>
+                <h1>404</h1>
+                <p>
+                    Ooops! <br /> Похоже, страница, которую вы ищете, не
+                    найдена!
+                </p>
+                <Button text="К предыдущей странице" onClick={handleClick} />
+
+                <Link to="/">На главную</Link>
+            </section>
+        </div>
+    );
 }
