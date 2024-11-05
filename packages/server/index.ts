663b7de9f9858eb275327de9faaadf8ca1cc9dcd
Daniel <113412945+anillen@users.noreply.github.com>
2024-11-05 09:33:47 +0300

    Revert "77. o auth"

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -6,25 +6,10 @@ import * as fs from 'fs';
 import { yandexApiProxyMiddleware } from './middlewares/yandexApiProxyMiddleware';
 import dotenv from 'dotenv';
 dotenv.config();
-import axios from 'axios';
-import express from 'express';
-import { createClientAndConnect } from './db';
-import { yandexApiProxyMiddleware } from './middlewares/yandexApiProxyMiddleware';
-import { GetServiceIdModel } from './models/GetServiceIdModel';
-
-const { CLIENT_URL, SERVER_URL, API_URL, SERVER_PORT } = process.env;
-console.log(CLIENT_URL, SERVER_URL, API_URL, SERVER_PORT);
-const app = express();
-app.use(
-    cors({
-        origin: CLIENT_URL,
-        optionsSuccessStatus: 200,
-        credentials: true,
-    }),
-);
-const port = Number(SERVER_PORT) || 3001;
 
 const isDev = () => process.env.NODE_ENV === 'development';
+
+const CLIENT_URL = 'http://localhost:3000';
 
 async function startServer() {
     const app = express();
@@ -35,8 +20,7 @@ async function startServer() {
             credentials: true,
         }),
     );
-  
-    const port = Number(SERVER_PORT) || 3001;
+    const port = Number(process.env.SERVER_PORT) || 3001;
 
     let vite: ViteDevServer | undefined;
     const distPath = path.dirname(require.resolve('client/dist/index.html'));
@@ -93,33 +77,6 @@ async function startServer() {
     });
 
     app.use(yandexApiProxyMiddleware);
-
-app.post('/api/yandex-callback', req => {
-    const code = req.body as string;
-    return axios
-        .post(`${process.env.API_URL}/api/v2/oauth/yandex`, {
-            code: code,
-            redirect_url: CLIENT_URL,
-        })
-        .catch(error => {
-            console.error('Error oauth authorize', error);
-            throw error;
-        });
-});
-
-app.post('/api/signin-by-yandex', (_, res) => {
-    axios
-        .get(`${API_URL}/api/v2/oauth/yandex/service-id`)
-        .then(result => {
-            const model = result.data as GetServiceIdModel;
-            const url = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${model.service_id}&redirect_uri=${SERVER_URL}/api/yandex-callback`;
-            res.json(url);
-        })
-        .catch(error => {
-            console.error(error);
-            throw error;
-        });
-});
 
     app.listen(port, () => {
         console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
--- packages/server/models/GetServiceIdModel.ts
+++
@@ -1,3 +0,0 @@
-export type GetServiceIdModel = {
-    service_id: string;
-};
