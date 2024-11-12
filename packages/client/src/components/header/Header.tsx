59b3d6684de431376fd57d5ce115052e6412862e
Дмитрий Токмак <dt@webizi.ru>
2024-11-12 10:18:31 +0300

    Вынес FullScreen в отдельный хук

--- packages/client/src/components/header/Header.tsx
+++ packages/client/src/components/header/Header.tsx
@@ -1,27 +1,19 @@
 import styles from './styles.module.scss';
 import { EPageRoutes } from '@/router/Enums';
 import { Button, IconAccount, IconHome, IconLink } from '@/ui';
-import { FC, useState } from 'react';
+import { FC } from 'react';
+import { useFullScreen } from '@/hooks/full-screen';
 
 export const Header: FC = () => {
-    const [fullscreen, setFullscreen] = useState(false);
-    function toggleFullScreen() {
-        if (!document.fullscreenElement) {
-            setFullscreen(true);
-            document.documentElement.requestFullscreen();
-        } else if (document.exitFullscreen) {
-            document.exitFullscreen();
-            setFullscreen(false);
-        }
-    }
+    const { isFullScreen, toggleFullScreen } = useFullScreen();
 
     return (
         <header className={styles.header}>
             <IconLink to={`/${EPageRoutes.HOME_PAGE}`} tooltip="На главную">
                 <IconHome width={24} height={24} />
             </IconLink>
-            <Button variant={fullscreen ? 'secondary' : 'primary'} onClick={toggleFullScreen}>
-                {fullscreen ? 'Выйти из полноэкранного режима' : 'Полноэкранный режим'}
+            <Button variant={isFullScreen ? 'secondary' : 'primary'} onClick={toggleFullScreen}>
+                {isFullScreen ? 'Выйти из полноэкранного режима' : 'Полноэкранный режим'}
             </Button>
             <div className={styles['header-right']}>
                 <IconLink to={`/${EPageRoutes.PROFILE_PAGE}`} tooltip="Профиль">
