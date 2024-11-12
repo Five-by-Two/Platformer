56a28ff4bc0ac0ea79077beaddb4ccc4b64bac65
Дмитрий Токмак <dt@webizi.ru>
2024-11-12 11:08:49 +0300

    Фикс конфликтного мержа

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -4,13 +4,16 @@ import { createServer as createViteServer, ViteDevServer } from 'vite';
 import express from 'express';
 import * as path from 'path';
 import * as fs from 'fs';
-import { yandexApiProxyMiddleware } from './middlewares/yandexApiProxyMiddleware';
 import dotenv from 'dotenv';
+import axios from 'axios';
+import { yandexApiProxyMiddleware } from './middlewares/yandexApiProxyMiddleware';
+import { GetServiceIdModel } from './models/GetServiceIdModel';
+
 dotenv.config();
 
 const isDev = () => process.env.NODE_ENV === 'development';
 
-const CLIENT_URL = 'http://localhost:3000';
+const { CLIENT_URL, SERVER_URL, API_URL, SERVER_PORT } = process.env;
 
 async function startServer() {
     const app = express();
@@ -21,7 +24,7 @@ async function startServer() {
             credentials: true,
         }),
     );
-    const port = Number(process.env.SERVER_PORT) || 3001;
+    const port = Number(SERVER_PORT) || 3001;
 
     let vite: ViteDevServer | undefined;
     const distPath = path.dirname(require.resolve('client/dist/index.html'));
@@ -83,6 +86,33 @@ async function startServer() {
     });
 
     app.use(yandexApiProxyMiddleware);
+
+    app.post('/api/yandex-callback', req => {
+        const code = req.body as string;
+        return axios
+            .post(`${process.env.API_URL}/api/v2/oauth/yandex`, {
+                code: code,
+                redirect_url: CLIENT_URL,
+            })
+            .catch(error => {
+                console.error('Error oauth authorize', error);
+                throw error;
+            });
+    });
+
+    app.post('/api/signin-by-yandex', (_, res) => {
+        axios
+            .get(`${API_URL}/api/v2/oauth/yandex/service-id`)
+            .then(result => {
+                const model = result.data as GetServiceIdModel;
+                const url = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${model.service_id}&redirect_uri=${SERVER_URL}/api/yandex-callback`;
+                res.json(url);
+            })
+            .catch(error => {
+                console.error(error);
+                throw error;
+            });
+    });
 
     app.listen(port, () => {
         console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
