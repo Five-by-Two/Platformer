6c6cb5fa5e06e387fbfc4146e4464be8b0f9b757
anillen <daniil_telenkov@vk.com>
2024-11-13 11:06:53 +0300

    add sequelize service and fix check connection

--- packages/server/db.ts
+++ packages/server/db.ts
@@ -1,27 +1,10 @@
-import { Client } from 'pg';
+import { SequelizeService } from './services/SequelizeService';
 
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_HOST, POSTGRES_PORT } = process.env;
-
-export const createClientAndConnect = async (): Promise<Client | null> => {
+export const createClientAndConnect = async () => {
     try {
-        const client = new Client({
-            user: POSTGRES_USER,
-            host: POSTGRES_HOST,
-            database: POSTGRES_DB,
-            password: POSTGRES_PASSWORD,
-            port: Number(POSTGRES_PORT),
-        });
-
-        await client.connect();
-
-        const res = await client.query('SELECT NOW()');
-        console.log('  ➜ 🎸 Connected to the database at:', res?.rows?.[0].now);
-        client.end();
-
-        return client;
-    } catch (e) {
-        console.error(e);
+        await SequelizeService.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
     }
-
-    return null;
 };
