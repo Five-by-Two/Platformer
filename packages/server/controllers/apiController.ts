0df23f2eb2735ba56e1b3bc50f75dfd17b114e53
anillen <daniil_telenkov@vk.com>
2024-11-14 14:18:12 +0300

    add reaction actions

--- packages/server/controllers/apiController.ts
+++ packages/server/controllers/apiController.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { commentController } from './commentController';
+import { reactionController } from './reactionController';
 import { replyController } from './replyController';
 import { topicController } from './topicController';
 
@@ -8,3 +9,4 @@ export const apiController = Router();
 apiController.use('/topics', topicController);
 apiController.use('/comments', commentController);
 apiController.use('/replies', replyController);
+apiController.use('/reactions', reactionController);
---
+++ packages/server/controllers/reactionController.ts
@@ -0,0 +1,29 @@
+import { Request, Response, Router } from 'express';
+import { CreateReactionDto } from '../dtos/CreateReactionDto';
+import ReactionService from '../services/ReactionService';
+
+export const reactionController = Router();
+
+reactionController.get('/by-comment/:commentId', (req: Request, res: Response) =>
+    ReactionService.getByCommentId(Number(req.params['commentId']))
+        .then(reactions => res.send(JSON.stringify(reactions)))
+        .catch(error => {
+            console.error(error);
+            res.sendStatus(500);
+        }),
+);
+reactionController.get('/:reacitonId', (req: Request, res: Response) =>
+    ReactionService.getByIdAsync(Number(req.params['reacitonId']))
+        .then(reaction => (reaction === null ? res.sendStatus(204) : res.send(JSON.stringify(reaction))))
+        .catch(error => {
+            console.error(error);
+            res.sendStatus(500);
+        }),
+);
+
+reactionController.post('/create', (req: Request, res: Response) => {
+    const model = req.body as CreateReactionDto;
+    ReactionService.createAsync(model)
+        .then(result => res.send(result))
+        .catch(error => res.status(400).send(JSON.stringify(error.message)));
+});
