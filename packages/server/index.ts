a00804d4f70bed7a83092cef80af8f4ecbf8981c
Дмитрий Токмак <dt@webizi.ru>
2024-11-21 15:41:58 +0300

    Docker

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,120 +1,20 @@
-import axios from 'axios';
-import cors from 'cors';
 import dotenv from 'dotenv';
-import express from 'express';
-import * as fs from 'fs';
-import * as path from 'path';
-import serialize from 'serialize-javascript';
-import { createServer as createViteServer, ViteDevServer } from 'vite';
-import { yandexApiProxyMiddleware } from './middlewares/yandexApiProxyMiddleware';
-import { GetServiceIdModel } from './models/GetServiceIdModel';
-
+import cors from 'cors';
 dotenv.config();
 
-const isDev = () => process.env.NODE_ENV === 'development';
+import express from 'express';
+import { createClientAndConnect } from './db';
 
-const { CLIENT_URL, SERVER_URL, API_URL, SERVER_PORT } = process.env;
+const app = express();
+app.use(cors());
+const port = Number(process.env.SERVER_PORT) || 3001;
 
-async function startServer() {
-    const app = express();
-    app.use(
-        cors({
-            origin: CLIENT_URL,
-            credentials: true,
-        }),
-    );
+createClientAndConnect();
 
-    const port = Number(SERVER_PORT) || 3001;
+app.get('/', (_, res) => {
+    res.json('👋 Howdy from the server :)');
+});
 
-    let vite: ViteDevServer | undefined;
-    const distPath = path.dirname(require.resolve('client/dist/index.html'));
-    const srcPath = path.dirname(require.resolve('client'));
-    const ssrClientPath = require.resolve('client/ssr-dist/client.cjs');
-
-    if (isDev()) {
-        vite = await createViteServer({
-            server: { middlewareMode: true },
-            root: srcPath,
-            appType: 'custom',
-        });
-
-        app.use(vite.middlewares);
-    }
-
-    if (!isDev()) {
-        app.use('/assets', express.static(path.resolve(distPath, 'assets')));
-    }
-
-    app.post('/api/yandex-callback', async req => {
-        const code = req.body as string;
-        try {
-            return await axios.post(`${process.env.API_URL}/api/v2/oauth/yandex`, {
-                code: code,
-                redirect_url: CLIENT_URL,
-            });
-        } catch (error) {
-            console.error('Error oauth authorize', error);
-            throw error;
-        }
-    });
-
-    app.post('/api/signin-by-yandex', (_, res) => {
-        axios
-            .get(`${API_URL}/api/v2/oauth/yandex/service-id`)
-            .then(result => {
-                const model = result.data as GetServiceIdModel;
-                const url = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${model.service_id}&redirect_uri=${SERVER_URL}/api/yandex-callback`;
-                res.json(url);
-            })
-            .catch(error => {
-                console.error(error);
-                throw error;
-            });
-    });
-    app.use(yandexApiProxyMiddleware);
-    app.use('*', async (req, res, next) => {
-        const url = req.originalUrl;
-
-        try {
-            let template: string;
-
-            if (!isDev()) {
-                template = fs.readFileSync(path.resolve(distPath, 'index.html'), 'utf-8');
-            } else {
-                template = fs.readFileSync(path.resolve(srcPath, 'index.html'), 'utf-8');
-
-                template = await vite!.transformIndexHtml(url, template);
-            }
-
-            let render: () => Promise<{ html: string; initialState: unknown }>;
-
-            if (!isDev()) {
-                render = (await import(ssrClientPath)).render;
-            } else {
-                render = (await vite!.ssrLoadModule(path.resolve(srcPath, 'ssr.tsx'))).render;
-            }
-
-            const { html: appHtml, initialState } = await render();
-            console.log(initialState);
-            const html = template.replace(`<!--ssr-outlet-->`, appHtml).replace(
-                `<!--ssr-initial-state-->`,
-                `<script>window.APP_INITIAL_STATE = ${serialize(initialState, {
-                    isJSON: true,
-                })}</script>`,
-            );
-
-            res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
-        } catch (e) {
-            if (isDev()) {
-                vite!.ssrFixStacktrace(e as Error);
-            }
-            next(e);
-        }
-    });
-
-    app.listen(port, () => {
-        console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
-    });
-}
-
-startServer();
+app.listen(port, () => {
+    console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
+});
