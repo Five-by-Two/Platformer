52d6a7610829349033c2faaf9afbef96147c3916
Timofey <tpich87@gmail.com>
2024-11-21 14:27:34 +0300

    update

--- packages/client/src/pages/Forum/models/forumTypes.ts
+++ packages/client/src/pages/Forum/models/forumTypes.ts
@@ -2,15 +2,6 @@ import { TComment, TTopic } from '@/store/forumSlice/types';
 
 export type Topic = TTopic;
 
-// {
-//     id: number;
-//     title: string;
-//     description: string;
-//     authorName: string;
-//     createdAt: string | Date;
-//     // comments: Comment[];
-// }
-
 export interface CreateTopicData {
     title: string;
     content: string;
