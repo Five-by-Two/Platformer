616445b101de9e7c2fcd7ae50fb47d69badad0ed
anillen <daniil_telenkov@vk.com>
2024-12-09 14:46:50 +0300

    fix env

--- packages/client/src/services/AxiosService/AxiosService.ts
+++ packages/client/src/services/AxiosService/AxiosService.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
-import { BASE_SERVER_URL } from './Models/Constants';
 
 export const AxiosService = axios.create({
-    baseURL: BASE_SERVER_URL,
+    baseURL: process.env.SERVER_URL,
     withCredentials: true,
 });
--- packages/client/src/services/AxiosService/Models/Constants.ts
+++
@@ -1,1 +0,0 @@
-export const BASE_SERVER_URL = process.env.SERVER_URL || 'http://localhost:3001';
